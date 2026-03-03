2974d0fb32ae4eb212d8ad3fa990eadf06257d96
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2026-03-03 16:02:46 +0700

    penambahan UserServiceImpl

--- praktikum1/src/main/java/com/example/praktikum1/repository/UserRepository.java
+++ praktikum1/src/main/java/com/example/praktikum1/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package com.example.praktikum1.repositori;
+package com.example.praktikum1.repository;
 
 import com.example.praktikum1.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
